e526f5487cb8647151c65073f41b13b5e483e145
LeoCosta <leocosta.black@hotmail.com>
2022-05-15 15:46:32 -0300

    Create 'secrets' file

---
+++ lib/auth/secrets.dart
@@ -0,0 +1,7 @@
+// Pegue uma chave de API em "https://openweathermap.org/full-price"
+// API que fornece informações sobre previsão do tempo
+const String weatherApiKey = '<API_KEY>';
+
+// Pegue uma chave de API em "https://positionstack.com/product"
+// API que fornece informações de localização (incluíndo coordenadas) com base em um endereço pesquisado
+const String positionstackApiKey = '<API_KEY>';
